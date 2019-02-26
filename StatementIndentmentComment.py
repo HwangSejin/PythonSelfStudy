d879264db467671b6a16674202f7de2023e7dd22
HwangSejin <snn.hwang@gmail.com>
2019-02-26 22:37:10 +0900

    Python Statement, Indentment and Comment

---
+++ StatementIndentmentComment.py
@@ -0,0 +1,43 @@
+a = 1 + 2 + 3 + \
+    4 + 5 + 6 + \
+    7 + 8 + 9
+print(a)
+
+n = (1 * 2 * 3 + 7 + 8 + 9)
+print(n)
+
+colors = ["Yellow",
+        "Red",
+        "Black"]
+print(colors)
+
+x = {1 + 2 + 3 + 4 + 5 + 6 +
+    7 + 8 + 9}
+print(x)
+
+flag = 2; ropes = 3; pole = 4
+
+site = "Hello"
+if site == "Hello":
+    print("Hello")
+else :
+    print("Wrong")
+print("Good job!")
+
+j = 1
+while(j<=5):
+    print(j)
+    j = j + 1
+
+print("Hello Python!")
+
+a, b = 1, 3
+sum = a + b
+print(sum)
+
+print("""Hello
+my name is
+Sejin""")
+
+print('''mutiple line
+can be written''')
